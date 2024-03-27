786ff6e2f17882537e9d4b018cd9b13bdf18fde0
Vikas Upadhayay <vikas.upadhayay@cginfinity.com>
2024-03-27 23:20:36 +0530

    Added router configuration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { Hono } from 'hono'
+import { Prisma, PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import userRoute from './route/auth'
+import statisticRoute from './route/statistic'
+import templateRoute from './route/template'
+import { cors } from 'hono/cors'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use('/*', cors());
+app.route('/api/v1/auth', userRoute);
+app.route('/api/v1/template', templateRoute);
+app.route('/api/v1/statistic', statisticRoute);
 
 export default app
---
+++ src/route/auth.ts
@@ -0,0 +1,42 @@
+import { Hono } from 'hono'
+import { Prisma, PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const userRoute = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>()
+
+userRoute.post('/config', async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+  
+    const body = await c.req.json();
+  
+    const user = await prisma.user.findUnique({
+          where: {
+              email: body.email
+          }
+      });
+  
+    if(!user)
+    {
+      try {
+        const user = await prisma.user.create({
+          data: {
+            email: body.email,
+            name: body.name
+          }
+        });
+        return c.json({ message: "configuration complete", userid : user.id });
+      } catch(e) {
+        c.status(403);
+        return c.json({ error: "error while configuration" });
+      }
+    }
+    return c.json({ message: "configuration complete", userid : user.id });
+  })
+  
+export default userRoute;
